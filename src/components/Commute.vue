3337a1d7977b0275d3ad5046974b34b1be900d1e
ManyKaufmann <manynouk.kaufmann@stud.hslu.ch>
2021-11-17 10:17:31 +0100

    upd

---
+++ src/components/Commute.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="commute">
+    <h1>{{ person }}'s commute</h1>
+    <div>{{ person }} commutes from {{ start }} to {{ end }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "commute",
+  props: {
+    person: String,
+    start: String,
+    end: String,
+  },
+};
+</script>
