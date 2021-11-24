e3387725ae8a09765cd34df776cd9d2dbac2973f
ManyKaufmann <manynouk.kaufmann@stud.hslu.ch>
2021-11-24 13:30:18 +0100

    upd contentful.js

--- src/modules/Contentful.js
+++ src/modules/Contentful.js
@@ -1,44 +1,28 @@
-import { createClient } from 'contentful'
+import { createClient } from "contentful";
 
 class Contentful {
+  constructor() {
+    this.client = createClient({
+      space: "ysi1w9hs8nqb",
+      accessToken: "VMBxczFLhJpJq09naVF2q44ubmFJ91Gm3098TrfYfuk",
+    });
+  }
 
-    constructor() {
-        this.client = createClient({
-            space: 'ysi1w9hs8nqb',
-            accessToken: 'VMBxczFLhJpJq09naVF2q44ubmFJ91Gm3098TrfYfuk'
-        })
-    }
+  async getHsluFacts() {
+    let result = await this.client.getEntries({
+      content_type: "hsluFacts",
+    });
+    console.log(result.items);
+    return result.items;
+  }
 
-    async getHsluFacts() {
-        let result = await this.client
-            .getEntries({
-                content_type: "hsluFacts"
-            });
-        const hsluDepartements = result.items.sort(
-            (teamA, teamB) => teamA.fields.reihenfolge - teamB.fields.reihenfolge,
-        )
-        console.log(hsluDepartements)
-        return hsluDepartements;
-    }
-
-    async getStart() {
-        let result = await this.client
-            .getEntries({
-                content_type: "start"
-            });
-        console.log(result.items);
-        return result.items;
-    }
-
-    async getDepartements() {
-        let result = await this.client
-            .getEntries({
-                content_type: "hsluFacts"
-            });
-        console.log(result.items);
-        return result.items.fields.departementName;
-    }
-
+  async getStart() {
+    let result = await this.client.getEntries({
+      content_type: "start",
+    });
+    console.log(result.items);
+    return result.items;
+  }
 }
 
 export default new Contentful();
