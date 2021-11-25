a93a37fad34b055aaf5d476b1f4cab635567874c
ManyKaufmann <manynouk.kaufmann@stud.hslu.ch>
2021-11-25 21:11:05 +0100

    upd about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,58 +1,19 @@
 <template>
-  <section class="about">
-    <h1>{{ spitzname }}</h1>
-    <img class="about-img" :src="aboutFoto.fields.file.url" />
-    <p>{{ aufgabe }}</p>
-     <div id="about-container">
-      <ul id="array-rendering">
-      <ul v-for="a in about" :key="a.about">
-          <About
-          :spitzname="a.fields.spitzname"
-          :aboutFoto="a.fields.aboutFoto"
-          :aufgabe="a.fields.aufgabe"
-          />
-      </ul>
-      </ul>
-      </div>
-  </section>
+  <div class="about-container">
+  </div>
 </template>
 
-<script>
-import contentful from "../modules/contentful.js";
-
-export default {
-  name: "About",
-  props: {
-    spitzname: String,
-    aboutFunktion: String,
-    aboutFoto: Object,
-    aufgabe: String,
-  },
-  
-
-  created: async function () {
-    this.about = await contentful.getAbout();
-  },
-};
-</script>
-
-
 <style scoped>
-.about {
-  font-family: "Spartan", sans-serif;
+.about-container {
   padding-top: 10px;
+  padding-bottom: 5px;;
+
   top: 0%;
-  height: 100vh;
+  height: 110vh;
   width: 100vw;
+  background-image: url("../assets/About.svg");
   background-color: rgba(255, 226, 102, 0.7);
+  background-size: cover;
   backdrop-filter: blur(7px);
 }
-
-  .about-img {
-  position: relative;
-  padding: 50px 50px;
-}
-
 </style>
-
-
